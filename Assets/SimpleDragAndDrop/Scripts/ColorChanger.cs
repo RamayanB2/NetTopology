f1438371a61617d0e2065588e736aed50fc7a19c
RamayanB2 <ramayanb2@gmail.com>
2018-01-16 20:00:47 -0200

    Initial commit

---
+++ Assets/SimpleDragAndDrop/Scripts/ColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    public DragAndDropItem[] setas_originais;
+    public Color[] colors;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ChangeColor(int j)
+    {
+        for (int i=0;i<setas_originais.Length;i++) {
+            setas_originais[i].GetComponent<UnityEngine.UI.Image>().color = colors[j];
+        }
+    }
+}
---
+++ Assets/SimpleDragAndDrop/Scripts/NameChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NameChanger : MonoBehaviour {
+
+    public UnityEngine.UI.InputField inputField;
+    string actual_name;
+
+    public DragAndDropItem ptt_original;
+    // Use this for initialization
+    void Start () {
+        inputField.text = "";
+        actual_name = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void ChangeName()
+    {
+        if (inputField.text != "" && inputField.text != actual_name) {
+            actual_name = inputField.text;
+            ptt_original.setName(actual_name);
+            Debug.Log("NAMECHANGED");
+        }
+    }
+}
